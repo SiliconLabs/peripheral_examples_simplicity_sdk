4dfe0a039fdf927726bf477a08d3cadcb2b4a19a
Spurthi Polavarapu <Spurthi.Polavarapu@silabs.com>
2025-04-09 04:06:53 +0000

    Adds extension support and BSP custom component for SLCP migration

---
+++ series2/kit/EFR32MG21_BRD4181A/pin_config.h
@@ -0,0 +1,39 @@
+// <<< sl:start pin_tool >>>
+
+// <gpio> LED0
+// $[GPIO_LED0]
+#define LED0_PORT SL_GPIO_PORT_B
+#define LED0_PIN  0
+// [GPIO_LED0]$
+
+// <gpio> LED1
+// $[GPIO_LED1]
+#define LED1_PORT SL_GPIO_PORT_B
+#define LED1_PIN  1
+// [GPIO_LED1]$
+
+// <gpio> BUTTON0
+// $[GPIO_BUTTON0]
+#define BUTTON0_PORT SL_GPIO_PORT_D
+#define BUTTON0_PIN  2
+// [GPIO_BUTTON0]$
+
+// <gpio> BUTTON1
+// $[GPIO_BUTTON1]
+#define BUTTON1_PORT SL_GPIO_PORT_D
+#define BUTTON1_PIN  3
+// [GPIO_BUTTON1]$
+
+// <gpio> EXP_UART_TX
+// $[GPIO_EXP_UART_TX]
+#define EXP_UART_TX_PORT SL_GPIO_PORT_A
+#define EXP_UART_TX_PIN  5
+// [GPIO_EXP_UART_TX]$
+
+// <gpio> EXP_UART_RX
+// $[GPIO_EXP_UART_RX]
+#define EXP_UART_RX_PORT SL_GPIO_PORT_A
+#define EXP_UART_RX_PIN  6
+// [GPIO_EXP_UART_RX]$
+
+// <<< sl:end pin_tool >>>
