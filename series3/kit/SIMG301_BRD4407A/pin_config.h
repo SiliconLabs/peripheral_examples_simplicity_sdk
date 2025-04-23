a0a55307dc00c6b4ab2f0060cababd900b5a92bf
Dayoung Lee <dalee@silabs.com>
2025-04-23 11:53:31 -0500

    Updates metadata files, adds bsp component

---
+++ series3/kit/SIMG301_BRD4407A/pin_config.h
@@ -0,0 +1,49 @@
+// LED active high
+#define LED_ON  1
+#define LED_OFF 0
+
+// <<< sl:start pin_tool >>>
+
+// <gpio> LED0
+// $[GPIO_LED0]
+#define LED0_PORT SL_GPIO_PORT_D
+#define LED0_PIN  3
+// [GPIO_LED0]$
+
+// <gpio> LED1
+// $[GPIO_LED1]
+#define LED1_PORT SL_GPIO_PORT_D
+#define LED1_PIN  4
+// [GPIO_LED1]$
+
+// <gpio> BUTTON0
+// $[GPIO_BUTTON0]
+#define BUTTON0_PORT SL_GPIO_PORT_B
+#define BUTTON0_PIN  1
+// [GPIO_BUTTON0]$
+
+// <gpio> BUTTON1
+// $[GPIO_BUTTON1]
+#define BUTTON1_PORT SL_GPIO_PORT_D
+#define BUTTON1_PIN  2
+// [GPIO_BUTTON1]$
+
+// <gpio> EXP_UART_TX
+// $[GPIO_EXP_UART_TX]
+#define EXP_UART_TX_PORT SL_GPIO_PORT_B
+#define EXP_UART_TX_PIN  2
+// [GPIO_EXP_UART_TX]$
+
+// <gpio> EXP_UART_RX
+// $[GPIO_EXP_UART_RX]
+#define EXP_UART_RX_PORT SL_GPIO_PORT_B
+#define EXP_UART_RX_PIN  0
+// [GPIO_EXP_UART_RX]$
+
+// <gpio> WDOG_PRS
+// $[GPIO_WDOG_PRS]
+#define WDOG_PRS_PORT SL_GPIO_PORT_C
+#define WDOG_PRS_PIN  1
+// [GPIO_WDOG_PRS]$
+
+// <<< sl:end pin_tool >>>
