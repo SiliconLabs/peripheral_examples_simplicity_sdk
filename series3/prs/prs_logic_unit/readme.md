cce95f0dc7011e32aecdb1680618ac7a04287242
Joe Stine <Joe.Stine@silabs.com>
2025-11-17 20:50:17 +0000

    Adds xG301 PRS example

---
+++ series3/prs/prs_logic_unit/readme.md
@@ -0,0 +1,48 @@
+# Peripheral Examples - PRS Logic Unit #
+
+## Summary ##
+
+This project demonstrates the built-in PRS logic functions between channels.
+Onboard push-buttons (PB0 and PB1) are set as PRS source. Onboard LED (LED1)
+is set as PRS output.
+
+Output uses the following truth table for OR logic function:
+PB1   PB0   |  OUTPUT 
+------------------------
+1     1     |     1     
+1     0     |     1    
+0     1     |     1     
+0     0     |     0  
+
+When the push button (PB0 or PB1) is pressed, a logical low is sampled by the
+GPIO and sent to the corresponding PRS channel
+
+When the push button (PB0 or PB1) is released, a logical high is sampled by the
+GPIO and sent to the corresponding PRS channel
+
+A logical low output will have the LED off and a logical high output will have
+the LED on.
+
+
+## Peripherals used ##
+
+* PRS
+* GPIO
+
+## How to Test ##
+
+1. Update the kit's firmware from the Simplicity Launcher (if necessary)
+2. Build the project and download to the Starter Kit
+3. Press the Push Buttons and notice how the LED1 responds.
+
+Note: Various other logic functions like AND, NAND, NOR, XOR, NOT etc are
+available. You can switch between these logic functions by changing the
+parameters passed when calling PRS_Combine() function in main.c.
+
+## Hardware & Connections ##
+
+* Board: Silicon Labs SixG301 Radio Board (BRD4407A) + Wireless Pro Kit Mainboard
+    * Device: SIMG301M104LIL
+        * PB01 - Button 0
+        * PD02 - Button 1
+        * PD03 - LED0
---
+++ series3/prs/prs_logic_unit/src/app.c
@@ -0,0 +1,86 @@
+/***************************************************************************//**
+ * @file
+ * @brief Top level application functions
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2025 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
+
+#include "sl_clock_manager.h"
+#include "sl_interrupt_manager.h"
+#include "sl_gpio.h"
+#include "sl_hal_prs.h"
+#include "pin_config.h"
+
+#define PRS_CH_A 6
+#define PRS_CH_B 1
+
+const sl_gpio_t GPIO_LED0 = { .port = LED0_PORT, .pin = LED0_PIN };
+const sl_gpio_t GPIO_BUTTON0 = { .port = BUTTON0_PORT, .pin = BUTTON0_PIN };
+const sl_gpio_t GPIO_BUTTON1 = { .port = BUTTON1_PORT, .pin = BUTTON1_PIN };
+
+/***************************************************************************//**
+ * Initialize GPIO.
+ ******************************************************************************/
+void gpio_init(void)
+{
+  // Initialize GPIO driver
+  sl_gpio_init();
+
+  // Configure LED0 as push-pull with output driving LED off
+  sl_gpio_set_pin_mode(&GPIO_LED0, SL_GPIO_MODE_PUSH_PULL, LED_OFF);
+
+  // Configure Buttons as input
+  sl_gpio_set_pin_mode(&GPIO_BUTTON0, SL_GPIO_MODE_INPUT_PULL, true);
+  sl_gpio_set_pin_mode(&GPIO_BUTTON1, SL_GPIO_MODE_INPUT_PULL, true);
+
+  // Configure Button pins as interrupt/prs signals
+  sl_hal_gpio_configure_external_interrupt(&GPIO_BUTTON0, BUTTON0_PIN, SL_GPIO_INTERRUPT_RISING_EDGE);
+  sl_hal_gpio_configure_external_interrupt(&GPIO_BUTTON1, BUTTON1_PIN, SL_GPIO_INTERRUPT_RISING_EDGE);
+
+}
+
+/***************************************************************************//**
+ * Initialize PRS.
+ ******************************************************************************/
+void prs_init(void)
+{
+  // Enable PRS bus clock
+  sl_clock_manager_enable_bus_clock(SL_BUS_CLOCK_PRS);
+
+  // Configure PRS channel A and B from interrupt/prs signals
+  sl_hal_prs_async_connect_channel_producer(PRS_CH_A, SL_HAL_PRS_ASYNC_GPIO_PIN1);
+  sl_hal_prs_async_connect_channel_producer(PRS_CH_B, SL_HAL_PRS_ASYNC_GPIO_PIN2);
+
+  // Combine channels with logical OR
+  sl_hal_prs_async_combine_signals(PRS_CH_A, PRS_CH_B, SL_HAL_PRS_LOGIC_A_OR_B);
+
+  // Route output to LED pin
+  sl_hal_prs_pin_output(PRS_CH_A,SL_HAL_PRS_TYPE_ASYNC, LED0_PORT, LED0_PIN);
+}
+
+/***************************************************************************//**
+ * Initialize application.
+ ******************************************************************************/
+void app_init(void)
+{
+  // Initialize GPIO and PRS
+  gpio_init();
+  prs_init();
+}
+
+/***************************************************************************//**
+ * App ticking function.
+ ******************************************************************************/
+void app_process_action(void)
+{
+}
