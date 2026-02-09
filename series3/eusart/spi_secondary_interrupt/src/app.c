744cba5f25d99d5b3ab367c1c81d5749f13b4bb0
Spurthi Polavarapu <Spurthi.Polavarapu@silabs.com>
2026-02-09 15:33:18 +0000

    Updates copyright year to 2025

--- series3/eusart/spi_secondary_interrupt/src/app.c
+++ series3/eusart/spi_secondary_interrupt/src/app.c
@@ -10,7 +10,7 @@
  * accompanying readme.txt file.
  *******************************************************************************
  * # License
- * <b>Copyright 2021 Silicon Laboratories Inc. www.silabs.com</b>
+ * <b>Copyright 2025 Silicon Laboratories Inc. www.silabs.com</b>
  *******************************************************************************
  *
  * SPDX-License-Identifier: Zlib
--- series3/gpcrc/software/src/app.c
+++ series3/gpcrc/software/src/app.c
@@ -3,7 +3,7 @@
  * @brief Use the GPCRC to check data
  *******************************************************************************
  * # License
- * <b>Copyright 2022 Silicon Laboratories Inc. www.silabs.com</b>
+ * <b>Copyright 2025 Silicon Laboratories Inc. www.silabs.com</b>
  *******************************************************************************
  *
  * SPDX-License-Identifier: Zlib
