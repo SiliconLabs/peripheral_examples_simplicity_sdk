d0001351ebc6dce05101126c80ccd191c3cc5be3
Austin Goodwin <Austin.Goodwin@silabs.com>
2026-04-22 20:44:34 +0000

    Updates DCDC VMON example and adds support for xG2D

--- series2/emu/dcdc_vmon/readme.md
+++ series2/emu/dcdc_vmon/readme.md
@@ -1,26 +1,53 @@
-# Peripheral Examples - DCDC VMON #
+# Peripheral Examples - DCDC Voltage Monitor #
 
-## Summary ##
-This project demonstrates the DCDC VREGVDD Threshold Comparator.
-The device will be switching between DCDC regulator on and DCDC bypass mode
-depending on the input voltage to VREGVDD as below:
-- Switch DCDC to bypass mode if VREGVDD is below 2.2 V
-- Enable DCDC if VREGVDD is above 2.2 V
+This project demonstrates using the DCDC VREGVDD comparator interrupt to change 
+DCDC regulation mode when above/below the configured VREGVDD input voltage threshold.
+
+This project initiates with the DCDC component in bypass mode. The VREGVDD comparator
+is used to monitor the DCDC input voltage (VREGVDD) and associated interrupt is used
+to switch the DCDC between bypass and regulation modes (for dual-output devices, 
+comparator interrupt switches between DECOUPLE only and dual-output modes).
+
+When the VREGVDD input is above the defined VREGVDD_THRESHOLD_HI threshold, single
+output DCDC devices will switch to regulating DVDD and dual-output devices will switch
+to dual-output mode regulating both DVDD and DECOUPLE. The VREGVDD threshold is also
+modified to VREGVDD_THRESHOLD_LOW adding some hysteresis to the thresholds.
+
+When VREGVDD input drops below VREGVDD_THRESHOLD_LOW threshold, the comparator interrupt
+reverts the threshold to VREGVDD_THRESHOLD_HI and switches DCDC mode to bypass for 
+single output DCDC devices and DECOUPLE only regulation mode for dual-output devices. 
+
+The VREGVDD comparator is active and generates interrupts in EM0 and EM1 only.
+
+The Power Manager software component allowing deep-sleep is used in this example 
+to drop to EM2. A periodic timer is used to wake the device and allow the interrupt
+to implement DCDC regulation changes as necessary. The periodic timer toggles LED1
+on the Wireless Pro Kit to simulate other firmware actions in a real-world application
+(RTOS scheduled tasks, e.g.). While in low energy mode EM2, current consumption is
+minimal and battery voltage is expected to remain steady. Battery voltage changes 
+most likely occur in higher energy modes, hence monitoring availability in only EM0/1.
+LED0 is used to indicate when the DCDC is in regulation for single output devices and 
+in dual-ouput mode for dual-output supported devices.
 
 ## Peripherals used ##
-EMU
+EMU/DCDC
 
 ## How to Test ##
 
 ### Basic testing ###
 1. Open Simplicity Studio and update the kit's firmware using **Device Manager Tool**(if necessary)
-2. Build the project and download to the Starter Kit
+2. Build the project and download to the Wireless Starter or Wireless Pro Kit and associated
 3. If debugging is active, terminate the debug session in your IDE to disconnect from the target device
-4. Place the Starter Kit power switch into BAT to disconnect the debugger's power
-5. Attach a power supply to GND and VMCU pins of the WSTK Expansion Header; Positive 3.30V should be applied to EXP pin 2 and 0.0V applied to EXP pin 1
+4. Place the kit's power switch into BAT to disconnect the debugger's power 
+5. Attach a power supply to VMCU and GND pins of the WSTK/WPK Expansion Header; Positive 3.30V
+   should be applied to EXP pin 2 and 0.0V applied to EXP pin 1
 6. Press the reset button the mainboard
-7. Vary the power supply voltage above/below the thresholds as per the instructions on the terminal program to start testing
-8. Observe the state of LED1. LED1 turns on when bypass mode is enabled and turns off again when bypass mode is disabled.
+7. Observe the periodic toggle of LED1 indicating that the firmware is running (LED brightness
+   will vary with supplied voltage
+8. Vary the power supply voltage above/below the defined VREGVDD_THRESHOLD_HI and 
+   VREGVDD_THRESHOLD_LOW thresholds
+9. Observe the state of LED0. LED0 turns on when below threshold and turns off when above; 
+   Corresponding DCDC regulation mode changes are summarized in the description above
 
 Note: When applying voltages independently to supply rails, please observe the operating conditions in the device's data sheet.
 
@@ -29,8 +56,8 @@ Note: When applying voltages independently to supply rails, please observe the o
 1. Follow steps 1, 2, and 3 above.
 2. While in the Simplicity Studio IDE, navigate to **Tools → Device Manager Tool**.
 3. Select your board, click **Configure**, then open **Terminal → Admin**.
-4. Change the VMCU supply voltage to 2 V by typing "target voltage 2 --nocalibrate" (no quotation marks) and pressing Enter. LED1 will turn on.
-5. Raise VMCU back to the typical 3.3 V by typing "target voltage 3.30 --nocalibrate" (again, no quotation marks) and pressing Enter. LED1 will turn off.
+4. Change the VMCU supply voltage to 2.0 V by typing "target voltage 2.0 --nocalibrate" (no quotation marks) and pressing Enter. LED0 will turn on. 
+5. Raise VMCU back to the typical 3.3 V by typing "target voltage 3.30 --nocalibrate" (again, no quotation marks) and pressing Enter. LED0 will turn off.
 6. When testing is complete, close the console tab in the IDE.
 
 ### Note for testing on xG25 ###
@@ -45,49 +72,73 @@ To test the DC-DC voltage monitoring example on BRD4270B, make the following mod
 ## Hardware & Connections ##
 
 * Board:  Silicon Labs EFR32xG22 Radio Board (BRD4182A) + Wireless Starter Kit Mainboard (BRD4001A)
-    * Device: EFR32MG22A224F512IM40
-        * VMCU - Expansion Header Pin 2
-        * GND  - Expansion Header Pin 1
-        * PD03  - LED1
+	* Device: EFR32MG22A224F512IM40
+		* VMCU - Expansion Header Pin 2
+		* GND  - Expansion Header Pin 1
+		* PB00 - Push Button PB0 (Escape Hatch)
+        * PD02 - LED0 - DCDC Comparator Output
+		* PD03 - LED1
 
 * Board:  Silicon Labs EFR32xG23 Radio Board (BRD4204D) + Wireless Starter Kit Mainboard (BRD4001A)
-    * Device: EFR32ZG23B010F512IM48
-        * VMCU - Expansion Header Pin 2
-        * GND  - Expansion Header Pin 1
-        * PD03  - LED1
+	* Device: EFR32ZG23B010F512IM48
+		* VMCU - Expansion Header Pin 2
+		* GND  - Expansion Header Pin 1 
+		* PB01 - Push Button PB0 (Escape Hatch)
+        * PB02 - LED0 - DCDC Comparator Output
+		* PD03 - LED1
 
 * Board:  Silicon Labs EFR32xG24 Radio Board (BRD4186C) + Wireless Starter Kit Mainboard (BRD4001A)
-    * Device: EFR32MG24B210F1536IM48
-        * VMCU - Expansion Header Pin 2
-        * GND  - Expansion Header Pin 1
-        * PB04  - LED1
+	* Device: EFR32MG24B210F1536IM48
+		* VMCU - Expansion Header Pin 2
+		* GND  - Expansion Header Pin 1 
+		* PB01 - Push Button PB0 (Escape Hatch)
+        * PB02 - LED0 - DCDC Comparator Output
+		* PB04 - LED1
 
 * Board:  Silicon Labs EFR32xG25 Radio Board (BRD4270B) + Wireless Starter Kit Mainboard (BRD4001A)
-    * Device: EFR32FG25B222F1920IM56
-        * VMCU - Expansion Header Pin 2
-        * GND  - Expansion Header Pin 1
-        * PC07  - LED1
+	* Device: EFR32FG25B222F1920IM56
+		* VMCU - Expansion Header Pin 2
+		* GND  - Expansion Header Pin 1
+		* PB00 - Push Button PB0 (Escape Hatch)
+        * PC06 - LED0 - DCDC Comparator Output
+		* PC07 - LED1
 
 * Board:  Silicon Labs EFR32xG26 Radio Board (BRD4117A) + Wireless Starter Kit Mainboard (BRD4001A)
-    * Device: EFR32MG26B420F3200IM48
-        * VMCU - Expansion Header Pin 2
-        * GND  - Expansion Header Pin 1
-        * PB04 -  LED1
+	* Device: EFR32MG26B420F3200IM48
+		* VMCU - Expansion Header Pin 2
+		* GND  - Expansion Header Pin 1
+		* PB01 - Push Button PB0 (Escape Hatch)
+        * PB02 - LED0 - DCDC Comparator Output
+		* PB04 - LED1
 
 * Board:  Silicon Labs EFR32xG27 Radio Board (BRD4194A) + Wireless Starter Kit Mainboard (BRD4001A)
-    * Device: EFR32MG27C140F768IM40
-        * VMCU - Expansion Header Pin 2
-        * GND  - Expansion Header Pin 1
-        * PB01  - LED1
+	* Device: EFR32MG27C140F768IM40
+		* VMCU - Expansion Header Pin 2
+		* GND  - Expansion Header Pin 1 
+		* PB00 - Push Button PB0 (Escape Hatch)
+        * PB00 - LED0 - DCDC Comparator Output
+		* PB01 - LED1
 
 * Board:  Silicon Labs EFR32xG28 Radio Board (BRD4400C) +Wireless Starter Kit Mainboard (BRD4001A)
-    * Device: EFR32ZG28B312F1024IM68
-        * VMCU - Expansion Header Pin 2
-        * GND  - Expansion Header Pin 1
-        * PD03  - LED1
+	* Device: EFR32ZG28B312F1024IM68
+		* VMCU - Expansion Header Pin 2
+		* GND  - Expansion Header Pin 1 
+		* PB01 - Push Button PB0 (Escape Hatch)
+        * PB02 - LED0 - DCDC Comparator Output
+		* PD03 - LED1
 
 * Board:  Silicon Labs EFR32xG29 Wireless 2.4 GHz 8 dBm Buck Radio Board (BRD4412A) + Wireless Starter Kit Mainboard (BRD4001A)
-    * Device: EFR32MG29B140F1024IM40
-        * VMCU - Expansion Header Pin 2
-        * GND  - Expansion Header Pin 1
-        * PB01 - LED1
+	* Device: EFR32MG29B140F1024IM40
+		* VMCU - Expansion Header Pin 2
+		* GND  - Expansion Header Pin 1 
+		* PB00 - Push Button PB0 (Escape Hatch)
+        * PB00 - LED0 - DCDC Comparator Output
+		* PB01 - LED1
+		
+* Board:  Silicon Labs EFR32FG2D 868-915 MHz 14 dBm Radio Board (BRD4277A) + Wireless Starter Kit Mainboard
+    * Device: EFR32FG2DB010F512IM48
+		* VMCU - Expansion Header Pin 2
+		* GND  - Expansion Header Pin 1
+        * PB01 - Push Button PB0 (Escape Hatch)
+        * PB02 - LED0 - DCDC Comparator Output
+		* PD03 - LED1
--- series2/emu/dcdc_vmon/src/app.c
+++ series2/emu/dcdc_vmon/src/app.c
@@ -16,27 +16,118 @@
  ******************************************************************************/
 
 #include "em_emu.h"
-#include "pin_config.h"
 #include "peripheral_config.h"
-#include "sl_interrupt_manager.h"
 #include "sl_gpio.h"
 
-// test mode control signals
+#include "sl_interrupt_manager.h"
+#include "sl_power_manager.h"
+#include "sl_sleeptimer.h"
+
+#include "pin_config.h"
+
+// Test mode control signals
 volatile bool low_voltage = false;
+volatile bool dcdc_interrupt_flag = false;
+
+#define VREGVDD_THRESHOLD_HI emuVreginCmpThreshold_2v3
+#define VREGVDD_THRESHOLD_LOW emuVreginCmpThreshold_2v1
+
+#ifndef BUTTON0_PORT
+  #define BUTTON0_PORT LED0_BUTTON0_PORT
+  #define BUTTON0_PIN LED0_BUTTON0_PIN
+#endif
+
+#ifndef LED0_PORT
+  #define LED0_PORT LED0_BUTTON0_PORT
+  #define LED0_PIN LED0_BUTTON0_PIN
+#endif
 
 #ifndef LED1_PORT
   #define LED1_PORT LED1_BUTTON1_PORT
   #define LED1_PIN LED1_BUTTON1_PIN
 #endif
 
+// Conditional compilation to configure GPIO mappings based on the target microcontroller
+#if defined(EFR32MG21A010F1024IM32)
+const sl_gpio_t GPIO_ESCAPE_HATCH = { .port = BUTTON1_PORT, .pin = BUTTON1_PIN };
+#else
+const sl_gpio_t GPIO_ESCAPE_HATCH = { .port = BUTTON0_PORT, .pin = BUTTON0_PIN };
+#endif
+
+const sl_gpio_t LED0 = { .port = LED0_PORT, .pin = LED0_PIN };
 const sl_gpio_t LED1 = { .port = LED1_PORT, .pin = LED1_PIN };
+
+sl_sleeptimer_timer_handle_t periodic_timer;
+
+void periodic_timer_callback(sl_sleeptimer_timer_handle_t *handle, void *data)
+{
+  (void)handle;
+  (void)data;
+  
+  // Code executed when the timer expires.
+  sl_gpio_toggle_pin(&LED1);
+}
+
+int start_timer(void)
+{
+  sl_status_t status;
+  uint32_t timer_timeout = 32768;
+
+  // We assume the sleeptimer is initialized properly
+  status = sl_sleeptimer_start_periodic_timer(&periodic_timer,
+                                     timer_timeout,
+                                     periodic_timer_callback,
+                                     (void *)NULL,
+                                     0,
+                                     0);
+  if(status != SL_STATUS_OK) {
+    return -1;
+  }
+  return 1;
+}
+
+/**************************************************************************//**
+ * When developing or debugging code that enters EM2 or lower, it's a
+ * good idea to have an "escape hatch" type mechanism, e.g. a way to
+ * pause the device so that a debugger can connect in order to erase
+ * flash, among other things.
+ *
+ * Before proceeding with this example, make sure Escape Hatch button is not
+ * pressed. If the Escape Hatch button pin is low, turn on LED0 and execute the
+ * breakpoint (BKPT) instruction to stop the processor in EM0 and allow a debug
+ * connection to be made.
+ *****************************************************************************/
+void escapeHatch(void)
+{
+  bool pin_value;
+
+  // Initialize GPIO driver
+  sl_gpio_init();
+
+  // Configure Escape Hatch button as an input pulled high
+  sl_gpio_set_pin_mode(&GPIO_ESCAPE_HATCH, SL_GPIO_MODE_INPUT_PULL_FILTER, true);
+
+  // Check Escape Hatch button state: if pressed, enable LED0 and trigger breakpoint
+  sl_gpio_get_pin_input(&GPIO_ESCAPE_HATCH, &pin_value);
+  if (pin_value == 0)
+  {
+    sl_gpio_set_pin_mode(&LED1, SL_GPIO_MODE_PUSH_PULL, LED_ON);
+    __BKPT(0);  // Halt execution for debugging
+  }
+  else
+  {
+    // Disable Escape Hatch button digital input when not in use
+    sl_gpio_set_pin_mode(&GPIO_ESCAPE_HATCH, SL_GPIO_MODE_DISABLED, false);
+  }
+}
 
 /**************************************************************************//**
  * @brief  GPIO Initializer
  *****************************************************************************/
 void gpio_init(void)
 {
-  // Configure LED1 pin as an output
+  // Configure indicator pins as outputs
+  sl_gpio_set_pin_mode(&LED0, SL_GPIO_MODE_PUSH_PULL, LED_OFF);
   sl_gpio_set_pin_mode(&LED1, SL_GPIO_MODE_PUSH_PULL, LED_OFF);
 }
 
@@ -45,20 +136,26 @@ void gpio_init(void)
  *****************************************************************************/
 void DCDC_IRQHandler(void)
 {
-  volatile uint32_t flag = DCDC -> IF;
+  volatile uint32_t flags = DCDC->IF;
 
-  // Clear all interrupt flag
-  DCDC -> IF_CLR = flag;
+  // Clear all interrupt flags
+  DCDC->IF_CLR = flags;
 
   // Check for interrupt flag
-  if (flag & DCDC_IF_VREGINLOW) {
-    low_voltage = true;
-    DCDC -> IEN_CLR |= DCDC_IEN_VREGINLOW;
-  }
-  if (flag & DCDC_IF_VREGINHIGH) {
+  if((flags & DCDC_IF_VREGINHIGH) && (DCDC->IEN & DCDC_IEN_VREGINHIGH)) {
     low_voltage = false;
-    DCDC -> IEN_CLR |= DCDC_IEN_VREGINHIGH;
+    DCDC->IEN_CLR = DCDC_IEN_VREGINHIGH;
+  } else if((flags & DCDC_IF_VREGINLOW) && (DCDC->IEN & DCDC_IEN_VREGINLOW)) {
+    low_voltage = true;
+    DCDC->IEN_CLR = DCDC_IEN_VREGINLOW;
   }
+
+  /*
+  *  Indicate threshold interrupt to app_process_action()
+  *  EMU emlib APIs have lengthy timeouts when setting DCDC to bypass or 
+  *  regulation modes that must not be handled within an ISR.
+  */ 
+  dcdc_interrupt_flag = true;
 }
 
 /*****************************************************************************/
@@ -66,19 +163,16 @@ void DCDC_IRQHandler(void)
  *****************************************************************************/
 void dcdc_init(void)
 {
-  EMU_DCDCInit_TypeDef dcdcInit = EMU_DCDCINIT_DEFAULT;
-
-  // Change dcdc threshold voltage level to 2.2V
-  dcdcInit.cmpThreshold = emuVreginCmpThreshold_2v2;
-
-  // Initializing DCDC with regulator on
-  EMU_DCDCInit(&dcdcInit);
+  // DCDC component initialized in sl_main_init(); default threshold is 2.3V
+  // Set VREGVDD comparator high threshold to project's #define
+  EMU->VREGVDDCMPCTRL = (EMU->VREGVDDCMPCTRL && _EMU_VREGVDDCMPCTRL_VREGINCMPEN_MASK)
+   | (VREGVDD_THRESHOLD_HI << _EMU_VREGVDDCMPCTRL_THRESSEL_SHIFT);
 
   // Clear all DCDC interrupt
-  DCDC->IF_CLR |= _DCDC_IF_MASK;
+  DCDC->IF_CLR = _DCDC_IF_MASK;
 
-  // Enable VREGIN low detection interrupt
-  DCDC->IEN_SET |= DCDC_IEN_VREGINLOW;
+  // Enable VREGIN high detection interrupt
+  DCDC->IEN_SET = DCDC_IEN_VREGINHIGH;
 
   // Enable DCDC interrupt vector.
   sl_interrupt_manager_clear_irq_pending(DCDC_IRQn);
@@ -90,11 +184,17 @@ void dcdc_init(void)
  ******************************************************************************/
 void app_init(void)
 {
+  // Halt execution if Escape Hatch button is pressed before entering EM2
+  escapeHatch();
+
   // Initialize GPIO
   gpio_init();
 
   // Initialize DCDC
   dcdc_init();
+
+  // start periodic timer
+  start_timer();
 }
 
 /***************************************************************************//**
@@ -102,26 +202,66 @@ void app_init(void)
  ******************************************************************************/
 void app_process_action(void)
 {
-  // If below threshold, switch to bypass mode, turn on LED
-  // Enable interrupt flags to detect VREGIN > VTHRESHOLD
-  if (low_voltage) {
-    EMU_DCDCModeSet(emuDcdcMode_Bypass);
-#if (LED_ON == 1)
-    sl_gpio_set_pin(&LED1);
-#else 
-    sl_gpio_clear_pin(&LED1);
-#endif
-    DCDC -> IEN_SET |= DCDC_IEN_VREGINHIGH;
-  }
-  else {
-  // If above threshold, switch to regulator on mode, turn off LED
-  // Enable interrupt flags to detect VREGIN < VTHRESHOLD
-    EMU_DCDCModeSet(emuDcdcMode_Regulation);
-#if (LED_ON == 1)
-    sl_gpio_clear_pin(&LED1);
-#else 
-    sl_gpio_set_pin(&LED1);
-#endif
-    DCDC -> IEN_SET |= DCDC_IEN_VREGINLOW;
+  if (dcdc_interrupt_flag) {
+    /* 
+    *  If below threshold, switch to bypass mode (DECOUPLE only mode for 
+    *  dual-output DCDC), turn on LED, and enable interrupt flags to detect 
+    *  VREGVDD > VTHRESHOLD
+    */
+    if (low_voltage) {
+      // Set VREGVDD comparator threshold to VREGVDD_THRESHOLD_HI (adds some hysteresis)
+      EMU->VREGVDDCMPCTRL = (EMU->VREGVDDCMPCTRL && _EMU_VREGVDDCMPCTRL_VREGINCMPEN_MASK)
+      | (VREGVDD_THRESHOLD_HI << _EMU_VREGVDDCMPCTRL_THRESSEL_SHIFT);
+
+      EMU_DCDCModeSet(emuDcdcMode_Bypass);
+
+      // Conditional compilation to configure dual output based on the target microcontroller
+  #if defined(EFR32FG2DB010F512IM48)    
+      // Change regulation type to DECOUPLE
+      EMU_DCDCSetRegulationType(emuDcdcRegulationType_RegDEC);
+      
+      // Re-enable DCDC
+      EMU_DCDCModeSet(emuDcdcMode_Regulation);
+  #endif
+
+  #if (LED_ON == 1)
+      sl_gpio_set_pin(&LED0);
+  #else 
+      sl_gpio_clear_pin(&LED0);
+  #endif
+
+      DCDC->IEN_SET = DCDC_IEN_VREGINHIGH;
+    } else {
+      /*
+      *  If above threshold, switch to regulator on mode (DVDD and DECOUPLE for
+      *  dual-output DCDC), turn off LED, and enable interrupt flags to detect
+      *  VREGVDD < VTHRESHOLD
+      */
+      // Set VREGVDD comparator threshold to VREGVDD_THRESHOLD_LOW (adds some hysteresis)
+      EMU->VREGVDDCMPCTRL = (EMU->VREGVDDCMPCTRL && _EMU_VREGVDDCMPCTRL_VREGINCMPEN_MASK)
+      | (VREGVDD_THRESHOLD_LOW << _EMU_VREGVDDCMPCTRL_THRESSEL_SHIFT);
+
+      // Conditional compilation to configure dual output based on the target microcontroller
+  #if defined(EFR32FG2DB010F512IM48)
+      // Disable DCDC; DCDC must be disabled when changing regulation type
+      EMU_DCDCModeSet(emuDcdcMode_Bypass);
+
+      // Change regulation type to dual-output DVDD + DECOUPLE
+      EMU_DCDCSetRegulationType(emuDcdcRegulationType_RegDVDDDEC);
+  #endif     
+
+      // Re-enable DCDC
+      EMU_DCDCModeSet(emuDcdcMode_Regulation);
+
+  #if (LED_ON == 1)
+      sl_gpio_clear_pin(&LED0);
+  #else 
+      sl_gpio_set_pin(&LED0);
+  #endif
+
+      DCDC->IEN_SET = DCDC_IEN_VREGINLOW;
+    }
+  
+  dcdc_interrupt_flag = false;
   }
 }
